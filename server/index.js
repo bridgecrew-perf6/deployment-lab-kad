20f049d15430f8d4126bf057969aaa13e95e96d4
Katie Anderson <katherine.lizf@gmail.com>
2022-04-27 17:16:01 -0500

    update4

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../styles/index.html'))
 })
+
+app.use(express.static("styles"))
 
 const port = process.env.PORT || 4005
 
